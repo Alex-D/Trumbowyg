5b633231da067930026e54bfcb4955930ba66180
DEMODE Alexandre <demodealex@gmail.com>
2012-10-02 02:07:25 +0200

    Correction des urls des images

--- config.rb
+++ config.rb
@@ -4,4 +4,4 @@ http_path = '/'
 css_dir = '/js/editor-design/css/'
 sass_dir = '/js/editor-design/sass/'
 images_dir = '/js/editor-design/images/'
-output_style = :compressed
+#output_style = :compressed
