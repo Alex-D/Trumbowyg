b4208ee900f5104bcf319d75a9361e3f7f13ee32
DEMODE Alexandre <demodealex@gmail.com>
2012-10-02 02:56:06 +0200

    Reorganisation pour compass (again)

--- config.rb
+++
@@ -1,7 +0,0 @@
-environment = :development
-
-http_path = '/'
-css_dir = '/js/editor-design/css/'
-sass_dir = '/js/editor-design/sass/'
-images_dir = '/js/editor-design/images/'
-#output_style = :compressed
---
+++ js/editor-design/css/config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "img"
+javascripts_dir = "javascripts"
+output_style = :nested
+relative_assets=true
+line_comments = false
