a39fee5e6d1432ea4d3f184ef7f7dff8aaf3c4f2
DEMODE Alexandre <demodealex@gmail.com>
2012-10-13 16:26:08 +0200

    Smoothscroll

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('header nav a[href^="#"]').click(function(e){
+	scrollGoto($(this).attr('href'));
+	e.preventDefault();
+});
+
+function scrollGoto(cible){
+	var hauteur = ($(cible).length >= 1) ? (parseInt($(cible).offset().top) - parseInt($('#container').css('padding-top')) - 15)+"px" : 0;
+	$('html,body').animate({scrollTop: hauteur}, 750);
+}
