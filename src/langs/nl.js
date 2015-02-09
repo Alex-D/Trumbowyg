67c2c5914a8cfc047bfd18a6a4857a3283c84026
Danny Hiemstra <dannyhiemstra@gmail.com>
2015-02-09 16:53:23 +0100

    Added dutch localization file

---
+++ src/langs/nl.js
@@ -0,0 +1,55 @@
+/* ===========================================================
+ * nl.js
+ * Dutch translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : Danny Hiemstra
+ *          Github : https://github.com/dhiemstra
+ */
+
+jQuery.trumbowyg.langs.de = {
+    viewHTML:       "HTML bekijken",
+
+    formatting:     "Formatering",
+    p:              "Paragraaf",
+    blockquote:     "Citaat",
+    code:           "Code",
+    header:         "Titel",
+
+    bold:           "Vet",
+    italic:         "Cursief",
+    strikethrough:  "Doorhalen",
+    underline:      "Onderlijnen",
+
+    strong:         "Sterk",
+    em:             "Nadruk",
+    del:            "Verwijderd",
+
+    unorderedList:  "Ongenummerde lijst",
+    orderedList:    "Genummerde lijst",
+
+    insertImage:    "Plaatje invoegen",
+    insertVideo:    "Video invoegen",
+    link:           "Link",
+    createLink:     "Link maken",
+    unlink:         "Link verwijderen",
+
+    justifyLeft:    "Links uitlijnen",
+    justifyCenter:  "Centreren",
+    justifyRight:   "Rechts uitlijnen",
+    justifyFull:    "Spreiden",
+
+    horizontalRule: "Horizontale lijn",
+
+    fullscreen:     "Volledig scherm",
+
+    close:          "Sluiten",
+
+    submit:         "Verzenden",
+    reset:          "Herstellen",
+
+    required:       "Verplicht",
+    description:    "Omschrijving",
+    title:          "Titel",
+    text:           "Tekst"
+};
