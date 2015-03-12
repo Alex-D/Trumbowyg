7bb6374668ef4b0652125d686776aec9ce70b7fa
Alex-D <demodealex@gmail.com>
2015-03-12 01:04:28 +0100

    Add anchors + end of redesign

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 hljs.initHighlightingOnLoad();
 
 (function($, undefined){
-	$(".cross-projects-open-button").on("click", function(){
-		$(".cross-projects-list").slideToggle(200);
-	});
+    $(".cross-projects-open-button").on("click", function(){
+        $(".cross-projects-list").slideToggle(200);
+    });
+
+    // Add anchors
+    $('.feature h3[id]').each(function(){
+        $(this).after($('<a/>', {
+            text: '§',
+            'class': 'title-link',
+            href: '#' + $(this).attr('id'),
+            title: "Permalink to " + $(this).text()
+        }));
+    });
 })(jQuery);
