3741876e086ea9969b9da1fbf4c0a9c36cf10d7e
John Pozy <pampersdry@gmail.com>
2015-08-13 15:50:05 +0800

    Add malaysian translation

---
+++ src/langs/my.js
@@ -0,0 +1,57 @@
+/* ===========================================================
+ * my.js
+ * Malaysian translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : John Pozy (pampersdry)
+ *          Twitter : @pampersdry
+ *          Website : http://pampersdry.info
+ *          Github : https://github.com/JohnPozy
+ */
+
+jQuery.trumbowyg.langs.id = {
+    viewHTML:       "Lihat HTML",
+
+    formatting:     "Pemformatan",
+    p:              "Perenggan",
+    blockquote:     "Blockquote",
+    code:           "Kod",
+    header:         "Pengepala",
+
+    bold:           "Tebal",
+    italic:         "Condong",
+    strikethrough:  "Garis batal",
+    underline:      "Garis bawah",
+
+    strong:         "Kuat",
+    em:             "Condong",
+    del:            "Hapus",
+
+    unorderedList:  "Senarai tidak tertib",
+    orderedList:    "Senarai tertib",
+
+    insertImage:    "Masukkan imej",
+    insertVideo:    "Masukkan video",
+    link:           "Pautan",
+    createLink:     "Masukkan pautan",
+    unlink:         "Hapus pautan",
+
+    justifyLeft:    "Mewajarkan kiri",
+    justifyCenter:  "Mewajarkan tengah",
+    justifyRight:   "Mewajarkan kanan",
+    justifyFull:    "Mewajarkan kiri dan kanan",
+
+    horizontalRule: "Masukkan garis mendatar",
+
+    fullscreen:     "Skrin penuh",
+
+    close:          "Tutup",
+
+    submit:         "Hantar",
+    reset:          "Batal",
+
+    required:       "Diperlukan",
+    description:    "Perihal",
+    title:          "Tajuk",
+    text:           "Teks"
+};
