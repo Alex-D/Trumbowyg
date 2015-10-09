29a2655a28ef3807705108da6e713b8346038556
Alex-D <demodealex@gmail.com>
2015-10-09 02:14:14 +0200

    feat: update language list, plugin list, ...

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 hljs.initHighlightingOnLoad();
 
 (function($, undefined){
-    $(".cross-projects-open-button").on("click", function(){
-        $(".cross-projects-list").slideToggle(200);
+    $('.cross-projects-open-button').on('click', function(){
+        $('.cross-projects-list').slideToggle(200);
+    });
+
+    // Lang accordion
+    $('#lang-list-view-full').on('click', function(){
+        $('#lang-list-light').slideUp(100);
+        $('#lang-list-full').slideDown(350);
     });
 
     // Add anchors
@@ -11,7 +17,7 @@ hljs.initHighlightingOnLoad();
             text: '§',
             'class': 'title-link',
             href: '#' + $(this).attr('id'),
-            title: "Permalink to " + $(this).text()
+            title: 'Permalink to ' + $(this).text()
         }));
     });
 })(jQuery);
