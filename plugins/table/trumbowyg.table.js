2ec55df5611eb114e8f01960f338950ac5296d41
Lawrence <media@absalom.biz>
2016-04-27 10:43:20 +1000

    1.2 update of Table Plugin - Add Row/Add Columns functions

--- plugins/table/trumbowyg.table.js
+++ plugins/table/trumbowyg.table.js
@@ -1,6 +1,6 @@
 /* ===========================================================
- * trumbowyg.table.js v1.1
- * Upload plugin for Trumbowyg
+ * trumbowyg.table.js v1.2
+ * Table plugin for Trumbowyg
  * http://alex-d.github.com/Trumbowyg
  * ===========================================================
  * Author : Lawrence Meckan
@@ -20,6 +20,8 @@
         langs: {
             en: {
                 table: 'Create Table',
+                addro: 'Add Rows',
+                addco: 'Add Columns',
                 rows: 'Rows',
                 columns: 'Columns',
                 styler: 'Table Class',
@@ -27,6 +29,8 @@
             },
             sk: {
                 table: 'Vytvoriť tabuľky',
+                addro: 'Pridať riadok',
+                addco: 'Pridať stĺpec',               
                 rows: 'Riadky',
                 columns: 'Stĺpce',
                 styler: 'Tabuľku triedy',
@@ -34,6 +38,8 @@
             },
             fr: {
                 table: 'Créez Table',
+                addro: 'Ajout de ligne',
+                addco: 'Ajouter colonnes',
                 rows: 'Lignes',
                 columns: 'Colonnes',
                 styler: 'Classe de table',
@@ -41,6 +47,8 @@
             },
             cs: {
                 table: 'Vytvořit příkaz Table',
+                addro: 'Přidat řádek',
+                addco: 'Přidat sloupec',
                 rows: 'Řádky',
                 columns: 'Sloupce',
                 styler: 'Tabulku třída',
@@ -53,7 +61,7 @@
                 init: function (trumbowyg) {
                     trumbowyg.o.plugins.table = $.extend(true, {}, defaultOptions, trumbowyg.o.plugins.table || {});
 
-                    var btnDef = {
+                    var tableBuild = {
                         fn: function () {
                         trumbowyg.saveRange();
                         trumbowyg.openModalInsert(
@@ -95,7 +103,31 @@
                             });
                         }
                     };
-                    trumbowyg.addBtnDef('table', btnDef);
+
+                    var addRow = {
+                        fn: function () {
+                        trumbowyg.saveRange();
+                        var rower = $('<tr></tr>');
+                        trumbowyg.range.deleteContents();
+                        trumbowyg.range.insertNode(rower[0]);
+                        return true;
+
+                        }
+                    };
+
+                    var addColumn = {
+                        fn: function () {
+                        trumbowyg.saveRange();
+                        var columner = $('<td></td>');
+                        trumbowyg.range.deleteContents();
+                        trumbowyg.range.insertNode(columner[0]);
+                        return true;
+
+                        }
+                    };
+                    trumbowyg.addBtnDef('table', tableBuild);
+                    trumbowyg.addBtnDef('addro', addRow);
+                    trumbowyg.addBtnDef('addco', addColumn);
                 }
             }
         }
