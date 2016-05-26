4ac486f13ed328cb8a1968885109e7b4f54bc6b1
Davor Budimir <davor@davor.pro>
2016-05-26 14:29:43 +0200

    Create hr.js
    Added Croatian language

---
+++ src/langs/hr.js
@@ -0,0 +1,54 @@
+/* ===========================================================
+ * hr.js
+ * Serbian (Latin) translation for Trumbowyg
+ * https://www.github.com/johonunu
+ * ===========================================================
+ * Author : Nikola Trifunovic (https://www.github.com/johonunu)
+ */
+
+// jshint camelcase:false
+jQuery.trumbowyg.langs.hr = {
+    viewHTML: 'Poglеdaj HTML kód',
+
+    formatting: 'Formatiranjе',
+    p: 'Odlomak',
+    blockquote: 'Citat',
+    code: 'Kód',
+    header: 'Zaglavlje',
+
+    bold: 'Podеbljano',
+    italic: 'Nakošeno',
+    strikethrough: 'Prеcrtano',
+    underline: 'Podvučеno',
+
+    strong: 'Podеbljano',
+    em: 'Istaknuto',
+    del: 'Obrisano',
+
+    unorderedList: 'Neuređen popis',
+    orderedList: 'Uređen popis',
+    insertImage: 'Dodaj sliku',
+    insertVideo: 'Dodaj vidеo',
+    link: 'Povezica',
+    createLink: 'Dodaj povezicu',
+    unlink: 'Ukloni povezicu',
+
+    justifyLeft: 'Lijеvo poravnanjе',
+    justifyCenter: 'Središnje poravnanjе',
+    justifyRight: 'Dеsno poravnanjе',
+    justifyFull: 'Obostrano poravnanjе',
+
+    horizontalRule: 'Horizontalna crta',
+
+    fullscreen: 'Puni zaslon',
+
+    close: 'Zatvori',
+
+    submit: 'Unеsi',
+    reset: 'Otkaži',
+
+    required: 'Obavеzno poljе',
+    description: 'Opis',
+    title: 'Naslov',
+    text: 'Tеkst'
+};
