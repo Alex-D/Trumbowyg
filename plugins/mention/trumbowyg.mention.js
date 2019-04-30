a70f65fc823e12be5b0da482232138820b5a5caa
Alex-D <contact@alex-d.fr>
2019-04-30 14:05:20 +0200

    style: clean some plugin code

--- plugins/mention/trumbowyg.mention.js
+++ plugins/mention/trumbowyg.mention.js
@@ -12,13 +12,14 @@
     'use strict';
 
     var defaultOptions = {
-        source: '',
+        source: [],
         formatDropdownItem: formatDropdownItem,
         formatResult: formatResult
     };
 
     $.extend(true, $.trumbowyg, {
         langs: {
+            // jshint camelcase:false
             en: {
                 mention: 'Mention'
             },
@@ -26,7 +27,7 @@
                 mention: 'Nævn'
             },
             fr: {
-                mention: 'Mentioner'
+                mention: 'Mentionner'
             },
             ru: {
                 mention: 'Упомянуть'
@@ -40,6 +41,7 @@
             pt_br: {
                 mention: 'Menção'
             },
+            // jshint camelcase:true
         },
 
         plugins: {
@@ -60,32 +62,29 @@
     /**
      * Build dropdown list
      *
-     * @param   {Array}   items      Items
-     * @param   {object}  trumbowyg  Editor
+     * @param {Array}   items      Items
+     * @param {object}  trumbowyg  Editor
      *
-     * @return  {Array}
+     * @return {Array}
      */
     function buildDropdown(items, trumbowyg) {
         var dropdown = [];
 
-        // Check if source is an array
-        if (items.constructor === Array) {
-            $.each(items, function (i, item) {
-                var btn = 'mention-' + i,
-                    btnDef = {
-                        hasIcon: false,
-                        text: trumbowyg.o.plugins.mention.formatDropdownItem(item),
-                        fn: function () {
-                            trumbowyg.execCmd('insertHTML', trumbowyg.o.plugins.mention.formatResult(item));
+        $.each(items, function (i, item) {
+            var btn = 'mention-' + i,
+                btnDef = {
+                    hasIcon: false,
+                    text: trumbowyg.o.plugins.mention.formatDropdownItem(item),
+                    fn: function () {
+                        trumbowyg.execCmd('insertHTML', trumbowyg.o.plugins.mention.formatResult(item));
 
-                            return true;
-                        }
-                    };
+                        return true;
+                    }
+                };
 
-                trumbowyg.addBtnDef(btn, btnDef);
-                dropdown.push(btn);
-            });
-        }
+            trumbowyg.addBtnDef(btn, btnDef);
+            dropdown.push(btn);
+        });
 
         return dropdown;
     }
