ec6a2c6c284961ee2da05bc5557b3de383473127
SDuck4 <cotmddn4@gmail.com>
2019-05-13 00:02:56 +0900

    feat: add korean translation to upload plugin

--- plugins/upload/trumbowyg.upload.js
+++ plugins/upload/trumbowyg.upload.js
@@ -107,7 +107,12 @@
                 upload: 'Yükle',
                 file: 'Dosya',
                 uploadError: 'Hata'
-            }
+            },
+            ko: {
+                upload: '파일 넣기',
+                file: '파일',
+                uploadError: '에러'
+            },
         },
         // jshint camelcase:true
 
