8fb34bd2c3486048e437a81733bf601787ca22d2
Alex-D <contact@alex-d.fr>
2020-12-06 17:53:47 +0100

    docs: update Trumbowyg to 2.22.0

--- docs/demos/js/loader.js
+++ docs/demos/js/loader.js
@@ -1,4 +1,4 @@
-var baseURL = window.location.hostname.indexOf('github.') !== -1 ? '//rawcdn.githack.com/Alex-D/Trumbowyg/v2.19.1/' : '../../../';
+var baseURL = window.location.hostname.indexOf('github.') !== -1 ? '//rawcdn.githack.com/Alex-D/Trumbowyg/v2.22.0/' : '../../../';
 var styleLoadingContainer = document.querySelector('.loading-head');
 var scriptLoadingContainer = document.querySelector('.loading-body');
 
