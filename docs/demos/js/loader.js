74815e6bbee93d0b570cf033939dd16acaf673e5
Alex-D <contact@alex-d.fr>
2020-12-08 01:53:06 +0100

    docs: upgrade to 2.23.0

--- docs/demos/js/loader.js
+++ docs/demos/js/loader.js
@@ -1,4 +1,4 @@
-var baseURL = window.location.hostname.indexOf('github.') !== -1 ? '//rawcdn.githack.com/Alex-D/Trumbowyg/v2.22.0/' : '../../../';
+var baseURL = window.location.hostname.indexOf('github.') !== -1 ? '//rawcdn.githack.com/Alex-D/Trumbowyg/v2.23.0/' : '../../../';
 var styleLoadingContainer = document.querySelector('.loading-head');
 var scriptLoadingContainer = document.querySelector('.loading-body');
 
