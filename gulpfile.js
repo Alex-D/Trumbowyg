e6287a116bbf294cb7774379ab2908576e244e14
T. Almroth <info@tim-international.net>
2021-08-11 04:19:40 +0200

    + Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@
 const gulp = require('gulp'),
     del = require('del'),
     vinylPaths = require('vinyl-paths'),
-    $ = require('gulp-load-plugins')();
+    $ = require('gulp-load-plugins')(),
+    sourcemaps = require('gulp-sourcemaps'),
 
 const paths = {
     langs: ['src/langs/**.js', '!src/langs/en.js'],
@@ -109,12 +110,14 @@ const icons = function () {
 const styles = function () {
     return gulp.src(paths.styles)
         .pipe($.sass())
+        .pipe(sourcemaps.init())
         .pipe($.autoprefixer(['last 1 version', '> 1%', 'ff >= 20', 'ie >= 9', 'opera >= 12', 'Android >= 2.2'], {cascade: true}))
         .pipe($.header(banner, {pkg: pkg, description: 'Default stylesheet for Trumbowyg editor'}))
         .pipe(gulp.dest('dist/ui/'))
         .pipe($.size({title: 'trumbowyg.css'}))
         .pipe($.rename({suffix: '.min'}))
         .pipe($.minifyCss())
+        .pipe(sourcemaps.write('.'))
         .pipe($.header(bannerLight, {pkg: pkg}))
         .pipe(gulp.dest('dist/ui/'))
         .pipe($.size({title: 'trumbowyg.min.css'}));
