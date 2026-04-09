e531e1fec0d84e0fb08caa997cc4d99924fc090c
Michael Ryvkin <mpryvkin@users.noreply.github.com>
2026-04-09 14:36:35 -0400

    feat(base64,pasteimage): add `maxFileSize` option
    add #1007

--- plugins/pasteimage/trumbowyg.pasteimage.js
+++ plugins/pasteimage/trumbowyg.pasteimage.js
@@ -11,25 +11,40 @@
 (function ($) {
     'use strict';
 
+    var defaultOptions = {
+        maxFileSize: 0
+    };
+
     $.extend(true, $.trumbowyg, {
         plugins: {
             pasteImage: {
                 init: function (trumbowyg) {
+                    trumbowyg.o.plugins.pasteImage = trumbowyg.o.plugins.pasteImage || defaultOptions;
+
                     trumbowyg.pasteHandlers.push(function (pasteEvent) {
                         try {
                             var items = (pasteEvent.originalEvent || pasteEvent).clipboardData.items,
                                 mustPreventDefault = false,
-                                reader;
+                                reader,
+                                file;
 
                             for (var i = items.length - 1; i >= 0; i -= 1) {
                                 if (items[i].type.match(/^image\//)) {
+                                    file = items[i].getAsFile();
+
+                                    // Validate file size
+                                    var maxFileSize = trumbowyg.o.plugins.pasteImage.maxFileSize || 0;
+                                    if (maxFileSize > 0 && file.size > maxFileSize) {
+                                        continue;
+                                    }
+
                                     reader = new FileReader();
                                     /* jshint -W083 */
                                     reader.onloadend = function (event) {
                                         trumbowyg.execCmd('insertImage', event.target.result, false, true);
                                     };
                                     /* jshint +W083 */
-                                    reader.readAsDataURL(items[i].getAsFile());
+                                    reader.readAsDataURL(file);
 
                                     mustPreventDefault = true;
                                 }
